fff3c9cca5dd409f90b59daae2ab745153653e8a
word <chen@ubuntu>
2016-09-08 17:55:20 -0700

    提交sessionfactory

--- hibernate0-0/src/main/java/com/chen/hibernate/sessionfactory.java
+++ hibernate0-0/src/main/java/com/chen/hibernate/sessionfactory.java
@@ -1,5 +1,21 @@
 package com.chen.hibernate;
 
-public class sessionfactory {
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+public class sessionfactory {
+	private static SessionFactory sessionFactory = null;
+	
+	public void setUp() throws Exception{
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.configure().build();
+		
+		try{
+			sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		}catch(Exception e){
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
 }
