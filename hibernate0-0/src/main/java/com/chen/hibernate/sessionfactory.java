ad8147fc3e2f17316f6aab0be8124ddd6653c4e6
home <334972898@qq.com>
2016-09-10 21:09:07 +0800

    test

--- hibernate0-0/src/main/java/com/chen/hibernate/sessionfactory.java
+++ hibernate0-0/src/main/java/com/chen/hibernate/sessionfactory.java
@@ -8,14 +8,27 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 public class sessionfactory {
 	private static SessionFactory sessionFactory = null;
 	
+	
 	public void setUp() throws Exception{
 		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-				.configure().build();
+				//.configure("/resources/hibernate.cfg.xml")
+				.configure()
+				.build();
 		
 		try{
 			sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
 		}catch(Exception e){
 			StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
+	
+	public static void main(String[] args) {
+		sessionfactory s = new sessionfactory();
+		try {
+			s.setUp();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
