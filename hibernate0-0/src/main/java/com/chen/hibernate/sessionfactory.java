c877d3a7a696ed4acf651714de2c8747c555072f
home <334972898@qq.com>
2016-09-10 21:48:05 +0800

    总算能够得到sessionfactory了真是日了狗，配置文件的路径简直搞笑

--- hibernate0-0/src/main/java/com/chen/hibernate/sessionfactory.java
+++ hibernate0-0/src/main/java/com/chen/hibernate/sessionfactory.java
@@ -1,5 +1,8 @@
 package com.chen.hibernate;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
@@ -7,25 +10,32 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class sessionfactory {
 	private static SessionFactory sessionFactory = null;
-	
-	
-	public void setUp() throws Exception{
+
+	public void setUp() throws Exception {
+		File f = new File(
+				"/home/chen/git/project/hibernate/hibernate0-0/src/main/java/com/chen/hibernate/hibernate.cfg.xml");
+		if(!f.exists()){
+			return;
+		}
 		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-				//.configure("/resources/hibernate.cfg.xml")
-				.configure()
-				.build();
-		
-		try{
+				.configure(f).build();
+
+		try {
 			sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-		}catch(Exception e){
+		} catch (Exception e) {
 			StandardServiceRegistryBuilder.destroy(registry);
 		}
 	}
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws FileNotFoundException {
 		sessionfactory s = new sessionfactory();
 		try {
 			s.setUp();
+			if(sessionfactory.sessionFactory != null){
+				
+				
+				sessionfactory.sessionFactory.close();
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
